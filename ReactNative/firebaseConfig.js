cd3eaa5cf23599d755715f2d9318568097ae2286
almeida-cma <93884913+almeida-cma@users.noreply.github.com>
2024-09-26 19:43:43 -0300

    Update firebaseConfig.js

--- ReactNative/firebaseConfig.js
+++ ReactNative/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANr3gcQKLZggCD_nE5uxnJj_95gPbTnZc",
-  authDomain: "js-users.firebaseapp.com",
-  projectId: "js-users",
-  storageBucket: "js-users.appspot.com",
-  messagingSenderId: "916964957327",
-  appId: "1:916964957327:web:738f3e070b8d862be9e4fa"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Inicializando Firebase
